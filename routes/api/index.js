bd1f4aee04a45935b1a56a51704ef2bdf55392e3
codemodeactivate <jeff.zenko@gmail.com>
2023-06-25 10:14:47 -0400

    saving progress

---
+++ routes/api/index.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+
+const userRoutes = require('./useRoutes');
+const thoughtRoutes = require('./thoughtRoutes');
+
+router.use('/users', userRoutes);
+router.use('/thoughts', thoughtRoutes);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 const express = require('express');
-const mongoose = require('mongoose');
+//const mongoose = require('mongoose');
+const db = require('./config/connection');
+const routes = require('./routes');
+//const moment = require('moment');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 
-app.use(express.json());
+
 app.use(express.urlencoded({ extended: true }));
-
-
+app.use(express.json());
 app.use('/api', routes);
 
 

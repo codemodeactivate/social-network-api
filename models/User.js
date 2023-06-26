cbbc88a0c0774ce37bd35c30a806ca9a5a233bf3
codemodeactivate <jeff.zenko@gmail.com>
2023-06-26 10:41:47 -0400

    user model

---
+++ models/User.js
@@ -0,0 +1,44 @@
+const { truncate } = require('fs');
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: /.+\@.+\..+/
+        },
+        thoughts: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'Thought'
+            }
+        ],
+        friends: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true
+        },
+        id: false
+    });
+
+    userSchema.virtual('friendCount').get(function() {
+        return this.friends.length;
+    });
+
+    const User = mongoose.model('User', userSchema);
+
+    module.exports = User;

e80afa5ea9cdcad681d868ea2ee87bf479ceabd2
codemodeactivate <jeff.zenko@gmail.com>
2023-06-26 12:23:16 -0400

    Reaction Model

---
+++ models/Reaction.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const ReactionSchema = new mongoose.Schema(
+    {
+        reactionId: {
+            type: mongoose.Schema.Types.ObjectId,
+            default: () => new mongoose.Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxlength: 280
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (createdAtVal) => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+        }
+    },
+
+    {
+        toJSON: {
+            getters: true
+        },
+        id: false
+    }
+);
+
+module.exports = ReactionSchema;
